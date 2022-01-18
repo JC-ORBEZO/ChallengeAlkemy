52713606ffef62e9aaa34a07b69af01700ab79e6
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-18 20:12:47 -0300

    Add Migration.

---
+++ ChallengeAlkemy/Migrations/20220118230828_Inicial.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChallengeAlkemy.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pelicula",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "Date", nullable: false),
+                    Calificacion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pelicula", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personajes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Peso = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    Historia = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personajes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PeliculaPersonaje",
+                columns: table => new
+                {
+                    PeliculasId = table.Column<int>(type: "int", nullable: false),
+                    PersonajeAsociadoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PeliculaPersonaje", x => new { x.PeliculasId, x.PersonajeAsociadoId });
+                    table.ForeignKey(
+                        name: "FK_PeliculaPersonaje_Pelicula_PeliculasId",
+                        column: x => x.PeliculasId,
+                        principalTable: "Pelicula",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PeliculaPersonaje_Personajes_PersonajeAsociadoId",
+                        column: x => x.PersonajeAsociadoId,
+                        principalTable: "Personajes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeliculaPersonaje_PersonajeAsociadoId",
+                table: "PeliculaPersonaje",
+                column: "PersonajeAsociadoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PeliculaPersonaje");
+
+            migrationBuilder.DropTable(
+                name: "Pelicula");
+
+            migrationBuilder.DropTable(
+                name: "Personajes");
+        }
+    }
+}
