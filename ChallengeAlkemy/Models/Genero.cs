ce97b76124000dcf9d9ac83a4c8892b533600e58
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-18 20:06:49 -0300

    Agregue la clase Genero.

---
+++ ChallengeAlkemy/Models/Genero.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChallengeAlkemy.Models
+{
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        [Url]
+        public string Imagen { get; set; }
+        public List<Pelicula> Peliculas { get; set; }
+    }
+}
