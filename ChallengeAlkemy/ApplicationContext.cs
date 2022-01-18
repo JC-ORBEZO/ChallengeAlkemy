ef50ec96c59cddccc27bed1239f0e55f02dfbeb1
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-18 19:31:23 -0300

    Agregue la clase Personaje y estableci la conexion a la db.

---
+++ ChallengeAlkemy/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using ChallengeAlkemy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeAlkemy
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+        public DbSet<Personaje> Personajes { get; set; }
+    }
+}
---
+++ ChallengeAlkemy/Models/Personaje.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChallengeAlkemy.Models
+{
+    public class Personaje
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Imagen { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        [Range(0,150)]
+        public int Edad { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal Peso { get; set; }
+        public string Historia { get; set; }
+        //public List<Pelicula> Peliculas { get; set; }        
+    }
+}
