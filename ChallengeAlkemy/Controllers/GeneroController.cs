d1cfb793043d6fecc72126c1cd2c4ccb590212c9
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-22 12:35:59 -0300

    Agregue 2 tipos de Get en GeneroController.

--- ChallengeAlkemy/Controllers/GeneroController.cs
+++ ChallengeAlkemy/Controllers/GeneroController.cs
@@ -1,17 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChallengeAlkemy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ChallengeAlkemy.Controllers
 {
     [ApiController]
     [Route("api/Generos")]
-    public class GeneroController:ControllerBase
+    public class GeneroController : ControllerBase
     {
         private readonly ApplicationContext _context;
 
         public GeneroController(ApplicationContext context)
         {
             _context = context;
-        }        
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Genero>>> Get()
+        {
+            return await _context.Generos.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var existe = await _context.Generos.AnyAsync(x => x.Id == id);
+            if (!existe) return BadRequest($"No existe el Genero de Id: {id}");
+            return await _context.Generos.FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
+    
 }
