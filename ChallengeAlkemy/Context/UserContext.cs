eb573bea21c8f23e7e5ce4f67708f5d5b219244f
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-02-04 10:21:10 -0300

    Cree la clase User y el contexto UserContext.

---
+++ ChallengeAlkemy/Context/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using ChallengeAlkemy.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeAlkemy.Context
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        private const string Schema = "users";
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema(Schema);
+        }
+    }
+}
---
+++ ChallengeAlkemy/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChallengeAlkemy.Models
+{
+    public class User : IdentityUser
+    {
+        public bool IsActive { get; set; }
+    }
+}
